12877dfb908373efd6bd44c2f572bb94b49491c3
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-10-03 16:15:09 +0200

    text

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -7,6 +7,7 @@
 
                     <div class="card-body">
                         I'm an example component.
+                        <!-- commento a caso -->
                     </div>
                 </div>
             </div>
