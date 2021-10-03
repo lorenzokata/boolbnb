f373da53ad6f4e02fb5195714192d4e199b9b39f
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-10-03 16:00:04 +0200

    removed Str import from UserSeeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 class UserSeeder extends Seeder
